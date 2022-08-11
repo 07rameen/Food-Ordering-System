52b2cd8eeb536c237967d283716e2770be7238d6
rameen <rameenhamidd257@gmail.com>
2022-08-11 00:32:07 -0700

    add menu table and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_055803) do
+ActiveRecord::Schema.define(version: 2022_08_11_070935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2022_08_11_055803) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "menus", force: :cascade do |t|
+    t.bigint "restaurant_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
+  end
+
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "phoneNo"
@@ -57,4 +64,5 @@ ActiveRecord::Schema.define(version: 2022_08_11_055803) do
     t.index ["admin_user_id"], name: "index_restaurants_on_admin_user_id"
   end
 
+  add_foreign_key "menus", "restaurants"
 end
