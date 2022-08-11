fb4d5b98f97fb038cf3dbba4486d0dd2e32b6857
rameen <rameenhamidd257@gmail.com>
2022-08-11 03:38:49 -0700

    Created Deal-Menu-Items,Migrations and Associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_11_101952) do
+ActiveRecord::Schema.define(version: 2022_08_11_102325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 2022_08_11_101952) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "deal_menu_items", force: :cascade do |t|
+    t.bigint "deal_id", null: false
+    t.bigint "menu_item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["deal_id"], name: "index_deal_menu_items_on_deal_id"
+    t.index ["menu_item_id"], name: "index_deal_menu_items_on_menu_item_id"
   end
 
   create_table "deals", force: :cascade do |t|
@@ -122,6 +131,8 @@ ActiveRecord::Schema.define(version: 2022_08_11_101952) do
     t.index ["admin_user_id"], name: "index_restaurants_on_admin_user_id"
   end
 
+  add_foreign_key "deal_menu_items", "deals"
+  add_foreign_key "deal_menu_items", "menu_items"
   add_foreign_key "employees", "restaurants"
   add_foreign_key "menu_addons", "menu_items"
   add_foreign_key "menu_items", "menu_categories"
