935837f7ada2e728c845e7600cd2a54f5d4b90e5
rameen <rameenhamidd257@gmail.com>
2022-08-15 00:08:09 -0700

    Added scope for Closed Restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ApplicationRecord
     belongs_to :admin_user 
+    has_one :menu
+    has_many :employee
+    has_many :order 
+
+    scope :closedstatus, -> {
+        where(:status => "Closed")
+      }
 end
