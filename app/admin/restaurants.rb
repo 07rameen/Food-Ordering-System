b54240785d15a0cf6bf9d41af04f19305a741bf7
rameen <rameenhamidd257@gmail.com>
2022-08-15 00:02:10 -0700

    Modified restaurants in Super Admin for CRUD

--- app/admin/restaurants.rb
+++ app/admin/restaurants.rb
@@ -1,11 +1,32 @@
 ActiveAdmin.register Restaurant do
 
+  scope :closedstatus 
+
+  index do 
+    column :name
+    column "Contact",:phoneNo
+    column :rating 
+    column :cutPercent, :sortable => :cutPercent do |restaurant|
+      div :class => "alignment" do 
+        number_to_percentage restaurant.cutPercent 
+      end 
+      #for price do number_to_price helper 
+    end
+    column :location 
+    column :postalCode
+    column "Opening Time",:startTime
+    column "Closing Time",:EndTime
+    column :status
+    column :admin_user_id 
+
+  end
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :phoneNo, :rating, :cutPercent, :location, :postalCode, :startTime, :EndTime, :status, :admin_user_id
+   permit_params :name, :phoneNo, :rating, :cutPercent, :location, :postalCode, :startTime, :EndTime, :status, :admin_user_id
   #
   # or
   #
